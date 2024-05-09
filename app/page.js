2712d590121aa878b84c0311511c74002ce469d8
Yerdaulet Utarbayev <yerdauletu.03@gmail.com>
2024-05-10 02:53:38 +0600

    1

---
+++ app/page.js
@@ -0,0 +1,17 @@
+// 'use client'
+
+import Image from "next/image";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <div>
+      <p>auth</p>
+      <input type="text" />
+      <br />
+      <input type="text" />
+      <br />
+      <button>push</button>
+    </div>
+  );
+}
